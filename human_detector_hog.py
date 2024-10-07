f8d7f332c4063bbace59529a6d51fcd52dd68e0d
BRUCE LI <jhq2792@autuni.ac.nz>
2024-10-07 18:20:15 +1300

    not use efficient det

--- human_detector_hog.py
+++ human_detector_hog.py
@@ -3,60 +3,55 @@ Model: HOG
 Input: Live camera
 Requirements: HOG from cv2 library
 
-Status: works on laptop, poor accuracy
+Status: works on laptop, poor accuracy, compatible with raspi
 """
 
+from picamera2 import Picamera2
 import cv2
 import time
 
-# Use hog and local computer camera
+# Use hog and raspi camera
 
 # Initialize HOG descriptor for people detection
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# Initialize webcam (0 is the default camera index for your laptop)
-cap = cv2.VideoCapture(0)
+# Initialize pi camera code
+picam2 = Picamera2()
+picam2.configure(picam2.create_still_configuration(main={"size": (720, 360)}))
+picam2.start()
 
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    print("Error: Could not open webcam.")
-    exit()
 
 while True:
-    # Capture frame from webcam
-    ret, frame = cap.read()
+    # Capture frame from pi camera device
+    frame = picam2.capture_array()
 
-    if ret:
+    if frame is not None:
         start_time = time.time()
-
-        # Resize frame to improve frame rate (optional)
-        frame = cv2.resize(frame, (1280, 720))
-
-        # Convert frame to grayscale for HOG detector
+        
+        # Convert frame to grayscale for HOG compatibility
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-        # Perform pedestrian detection
-        # Perform pedestrian detection
+        
+        # Use model to detect people
         rects, weights = hog.detectMultiScale(
-            gray_frame, 
-            winStride=(4, 4), 
-            padding=(8, 8), 
-            scale=1.05,  # Lower the scale to handle bigger objects
+           gray_frame,
+           winStride=(4,4),
+           padding=(8,8),
+           scale=1.01           
         )
-
-        # Measure elapsed time for detections
+        
+        # Measure time for detections
         end_time = time.time()
         print("Elapsed time:", end_time - start_time)
-
-        # Draw bounding boxes for detected pedestrians
+        
+        # Display rectangles
         for i, (x, y, w, h) in enumerate(rects):
             if weights[i] < 0.7:
                 continue
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Display the frame with detected pedestrians
-        cv2.imshow("Webcam - Human Detection", frame)
+            cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+            
+            # Display frame
+            cv2.imshow("Human Detection", frame)
 
     # Exit condition
     k = cv2.waitKey(1)
@@ -64,5 +59,5 @@ while True:
         break
 
 # Cleanup
-cap.release()
+picam2.stop()
 cv2.destroyAllWindows()
